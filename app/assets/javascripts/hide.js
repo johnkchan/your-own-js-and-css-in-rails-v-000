3d945efb0a49e507de3adaae783e5417996e47ab
Jonathan Chan <johnkchan252@gmail.com>
2019-06-25 03:44:21 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,3 +1,5 @@
-function hideWhenClicked() {
-
+function hideWhenClicked(event) {
+  return event.target.innerHTML = '';
 }
+
+document.getElementById('hide_this').addEventListener('click', hideWhenClicked);
